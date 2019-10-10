08b84311265c54e0af7826a4ca879ba4bbfcc613
Marc-Antoine C <claude.marcantoine2@gmail.com>
2019-10-10 14:47:36 +0200

    Responsive navigation - burger menu

--- js/main.js
+++ js/main.js
@@ -4,10 +4,28 @@ const app = {
   init: () => {
     console.log('App initialisation');
     app.smoothScrolling();
+    app.navigationToggle();
   },
 
   smoothScrolling: () => {
     new SmoothScroll('a[href*="#"]');
+  },
+
+  navigationToggle: () => {
+    const menuIcon = document.querySelector('.menu-toggle');
+    const navigation = document.querySelector('.navigation');
+    const navigationLinks = document.querySelectorAll('.navigation__link');
+
+    menuIcon.addEventListener('click', () => {
+      menuIcon.classList.toggle('open');
+      navigation.classList.toggle('navigation--open');
+    });
+
+    navigationLinks.forEach(link => {
+      link.addEventListener('click', () => {
+        navigation.classList.remove('navigation--open');
+      });
+    })
   }
 };
 
