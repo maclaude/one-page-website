55744aecc9d104088e82b7105dbcc786ed754dc0
Marc-Antoine C <claude.marcantoine2@gmail.com>
2019-10-11 13:02:01 +0200

    Toggle header up & down on scroll

--- js/main.js
+++ js/main.js
@@ -2,9 +2,14 @@
 
 const app = {
   init: () => {
-    console.log('App initialisation');
+    // Global selector
+    app.header =  document.querySelector('.header')
+    app.navigation = document.querySelector('.navigation');
+    app.menuIcon = document.querySelector('.menu-toggle');
+    // Actions
     app.smoothScrolling();
     app.navigationToggle();
+    app.headerToggleOnScroll();
   },
 
   smoothScrolling: () => {
@@ -12,20 +17,39 @@ const app = {
   },
 
   navigationToggle: () => {
-    const menuIcon = document.querySelector('.menu-toggle');
-    const navigation = document.querySelector('.navigation');
     const navigationLinks = document.querySelectorAll('.navigation__link');
 
-    menuIcon.addEventListener('click', () => {
-      menuIcon.classList.toggle('open');
-      navigation.classList.toggle('navigation--open');
+    // Toogle dropdown navigation on icon click
+    app.menuIcon.addEventListener('click', () => {
+      app.menuIcon.classList.toggle('open');
+      app.navigation.classList.toggle('navigation--open');
     });
 
+    // Close dropdown navigation on link click
     navigationLinks.forEach(link => {
       link.addEventListener('click', () => {
-        navigation.classList.remove('navigation--open');
+        app.navigation.classList.remove('navigation--open');
       });
     })
+  },
+
+  headerToggleOnScroll: () => {
+    let lastScrollTop = 0;
+
+    window.addEventListener('scroll', () => {
+      let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+      if (scrollTop > lastScrollTop) {
+        app.header.classList.add('header--up');
+        app.menuIcon.classList.remove('open');
+        app.navigation.classList.remove('navigation--open');
+      } else {
+        app.header.classList.remove('header--up');
+      }
+
+      // Setting last scroll top position
+      lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+    });
   }
 };
 
